9895afa6891c4160be561fde044923e361c34f78
asongoficeandtea <mariam_5544@hotmail.com>
2022-11-05 15:59:10 +0200

    slight change to message displayed on homepage

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hey Mariam. You're learning and doing well</p>"
+    return "<p>Hey Mariam. You're learning and doing well. Brazil GP is next!!!</p>"
