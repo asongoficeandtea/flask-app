9ba1aa562eb31883ef291f7313e3294336be911b
asongoficeandtea <mariam_5544@hotmail.com>
2022-11-09 10:35:44 +0200

    game code has worked and number generator is working well.

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import random
 
-rand_number = random.randint(1, 100)
+rand_number = random.randint(1, 5)
+user_number = ""
 
-print(rand_number)
-
-
-""" number = input("Guess the number between:")
-print(number)
-"""
+while user_number != rand_number:
+    user_number = int(input("Guess the number: "))
+    print("Still not right: ", user_number)
+print(f"You got the right number: {rand_number} and {user_number}")
