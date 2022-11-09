41f4080c9bb1615b58cc349d9eceb1f14882c9f3
asongoficeandtea <mariam_5544@hotmail.com>
2022-11-09 19:35:19 +0200

    created a function to fulfill the purpose of the game

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 import random
 
-rand_number = random.randint(1, 100)
+
+def game(number):
+    random_number = random.randint(1, number)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess the number between 1 and {number}:  "))
+        if guess > random_number:
+            print("Too high")
+        elif guess < random_number:
+            print("Too low")
+        else:
+            print(f"You guessed the correct number which is {random_number}")
+
+
+game(25)
+
+
+""" rand_number = random.randint(1, 100)
 user_number = ""
 
 while user_number != rand_number:
@@ -10,3 +27,4 @@ while user_number != rand_number:
     if user_number < rand_number:
         print("Too low")
 print(f" The correct number is: {rand_number}. Well done")
+"""
