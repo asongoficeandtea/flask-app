327823e198ec32aa7c8ec92639bb8c43bfcdb64b
asongoficeandtea <mariam_5544@hotmail.com>
2022-11-09 10:43:58 +0200

    added clues to guide the guessing of the number

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ user_number = ""
 
 while user_number != rand_number:
     user_number = int(input("Guess the number: "))
-    print("Still not right: ", user_number)
+    if user_number > rand_number:
+        print("Too high")
+    if user_number < rand_number:
+        print("Too low")
 print(f"You got the right number: {rand_number} and {user_number}")
