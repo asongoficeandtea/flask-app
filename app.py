9c3139ba15d0e6ea7cc56add4a028bd8492bbeee
asongoficeandtea <mariam_5544@hotmail.com>
2022-11-09 10:08:18 +0200

    random number generator started

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import random
 
-number = input("Guess the number between:")
+rand_number = random.randint(1, 100)
+
+print(rand_number)
+
+
+""" number = input("Guess the number between:")
 print(number)
+"""
